e5e62e465e0850dd9386d84b3f7110023d3f2960
user <shubelkom@gmail.com>
2023-04-19 18:06:16 +0300

    Task 2.5 Added tests for DAO

--- core/src/test/java/com/es/core/dao/phone/PhoneDaoImplIT.java
+++ core/src/test/java/com/es/core/dao/phone/PhoneDaoImplIT.java
@@ -4,23 +4,32 @@ import com.es.core.model.phone.Phone;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
 import java.math.BigDecimal;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration("/context/applicationContext-core-test.xml")
 public class PhoneDaoImplIT {
 
+    @Value("${phones.per.page}")
+    private int phonesPerPage;
     @Resource
-    private PhoneDao phoneDao;
+    private PhoneDao phoneDao = new PhoneDaoImpl();
 
     private Phone phoneTest;
 
@@ -67,10 +76,8 @@ public class PhoneDaoImplIT {
 
         phoneDao.save(phoneTest);
         Long generatedId = phoneTest.getId();
-
         phoneTest.setModel("new_model");
         phoneDao.save(phoneTest);
-
         phone = phoneDao.get(generatedId);
 
         if (phone.isPresent()) {
@@ -84,34 +91,73 @@ public class PhoneDaoImplIT {
     }
 
     @Test
-    public void findAll_without_paging() {
-        int expectedListSize = 11;
-        int offset = 0;
-        int limit = Integer.MAX_VALUE;
+    public void findAll_without_sorting_and_paging() {
+        List<Phone> phones = phoneDao.findAll(0, Integer.MAX_VALUE, new String[]{"", "", "id", "ASC"});
 
-        List<Phone> phones = phoneDao.findAll(offset, limit);
-
-        assertEquals(expectedListSize, phones.size());
+        assertEquals(10, phones.size());
     }
+
     @Test
     public void findAll_with_paging() {
-        int expectedListSize = 10;
-        int offset = 0;
-        int limit = 10;
+        List<Phone> phones = phoneDao.findAll(0, phonesPerPage, new String[]{"", "", "id", "ASC"});
 
-        List<Phone> phones = phoneDao.findAll(offset, limit);
-
-        assertEquals(expectedListSize, phones.size());
+        assertEquals(phonesPerPage, phones.size());
     }
 
     @Test
-    public void findAll_with_too_big_offset() {
-        int expectedListSize = 0;
-        int offset = Integer.MAX_VALUE;
-        int limit = 10;
+    public void findAll_with_sorting() {
+        List<Phone> phones = phoneDao.findAll(0, phonesPerPage, new String[]{"", "", "id", "ASC"});
+        List<Phone> sortedPhonesActual = phoneDao.findAll(0, phonesPerPage, new String[]{"", "", "model", "ASC"});
+        List<Phone> sortedPhonesExpected = phones.stream()
+                .sorted(Comparator.comparing(Phone::getModel))
+                .collect(Collectors.toList());
 
-        List<Phone> phones = phoneDao.findAll(offset, limit);
+        for (int i = 0; i <= 6; i++) {
+            assertEquals(sortedPhonesActual.get(i).getModel(), sortedPhonesExpected.get(i).getModel());
+        }
+    }
 
-        assertEquals(expectedListSize, phones.size());
+    @Test
+    public void findAll_with_searching() {
+        String searchingCriteriaLower = "b";
+        String searchingCriteriaUpper = "B";
+        List<Phone> phones = phoneDao.findAll(0, Integer.MAX_VALUE, new String[]{"", "", "id", "ASC"});
+        List<Phone> searchedPhonesActual = phoneDao.findAll(0, Integer.MAX_VALUE,
+                new String[]{searchingCriteriaLower, searchingCriteriaLower, "id", "ASC"});
+        List<Phone> searchedPhonesExpected = phones.stream()
+                .filter(ph -> ph.getModel().contains(searchingCriteriaLower)
+                        || ph.getBrand().contains(searchingCriteriaLower)
+                        || ph.getModel().contains(searchingCriteriaUpper)
+                        || ph.getBrand().contains(searchingCriteriaUpper))
+                .collect(Collectors.toList());
+
+        for (Phone phone : searchedPhonesExpected) {
+            assertTrue(phone.getModel().contains(searchingCriteriaLower)
+                    || phone.getBrand().contains(searchingCriteriaLower)
+                    || phone.getModel().contains(searchingCriteriaUpper)
+                    || phone.getBrand().contains(searchingCriteriaUpper));
+        }
+
+        assertEquals(searchedPhonesExpected.size(), searchedPhonesActual.size());
+        assertNotEquals(searchedPhonesExpected.size(), phones.size());
+    }
+
+
+    @Test
+    public void count_without_searching() {
+        List<Phone> phones = phoneDao.findAll(0, Integer.MAX_VALUE, new String[]{"", "", "id", "ASC"});
+
+        int count = phoneDao.count("");
+
+        assertEquals(count, phones.size());
+    }
+
+    @Test
+    public void count_with_searching() {
+        List<Phone> phones = phoneDao.findAll(0, Integer.MAX_VALUE, new String[]{"b", "b", "id", "ASC"});
+
+        int count = phoneDao.count("b");
+
+        assertEquals(count, phones.size());
     }
 }
